c0d493c29654db5f552cddb08d83aad8856af3e2
musimarcin <marcinm994@gmail.com>
2023-09-07 21:15:39 +0200

    Added function to determine computer's choice in string and reevaluate chosen weapon to negative value if needed. Added function to play the game.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,3 +20,50 @@ function getPlayerChoice() {
         }
     } while (x==0);
 }
+
+function reverseValue(computerChoice, playerChoice) {
+    computerChoice = getComputerChoice();
+    playerChoice = getPlayerChoice();
+
+    if (playerChoice==0 && computerChoice==2) {
+        return computerChoice = -computerChoice;
+    } else if (computerChoice==0 && playerChoice==2) {
+        return playerChoice = -playerChoice;
+    }
+}
+
+function computerChoicetoString(computerChoice) {
+    computerChoice = getComputerChoice();
+
+    let computerChoice_string = "";
+    switch (computerChoice) {
+        case 0: return computerChoice_string = "rock";
+            break;
+        case 1: return computerChoice_string = "paper";
+            break;
+        case 2: return computerChoice_string = "scissors";
+            break;
+        default:
+    }
+}
+
+function playRound(computerChoice, playerChoice) {
+
+    computerChoice = getComputerChoice();
+    playerChoice = getPlayerChoice();
+
+    switch (playerChoice) {
+        case "rock": playerChoice = 0;
+        break;
+        case "paper": playerChoice = 1;
+        break;
+        case "scissors": playerChoice = 2;
+        break;
+        default:
+            console.log("this is default")
+    }
+
+computerChoicetoString(computerChoice);
+reverseValue(computerChoice, playerChoice);
+
+}
