180e7f3dff113fc88356998621719f59b6d4adc7
musimarcin <marcinm994@gmail.com>
2023-09-11 18:31:32 +0200

    Removed do while loop from getPlayerChoice and replaced it to read input box from page instead of prompt. Added function to count score of the game.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice() {
  /*
     const array = new Int8Array(1);
@@ -8,9 +10,9 @@ function getComputerChoice() {
 
 
 function getPlayerChoice() {
-    let x=0;
-    do {
-        let player_choice = prompt("choose your weapon(rock/paper/scissors):").toLowerCase();
+    //let player_choice = prompt("choose your weapon(rock/paper/scissors):").toLowerCase();
+    let player_choice = document.getElementById("player_input").value.toString().toLowerCase();
+
         if (player_choice == "rock" || player_choice == "paper" || player_choice == "scissors") {
             switch (player_choice) {
                 case "rock": player_choice = 0;
@@ -21,19 +23,13 @@ function getPlayerChoice() {
                     break;
                 default:
             }
-                x=1;
              return player_choice;
         } else {
             alert("wrong weapon try again");
         }
-    } while (x==0);
 }
 
-function reverseValue(reverseComputer, reversePlayer) {
 
-
-    console.log(reverseComputer);
-}
 
 function computerChoiceToString(computerString) {
 
@@ -64,13 +60,53 @@ function playRound(computerChoice, playerChoice) {
         playerChoice = -playerChoice;
     }
 
+    let tie = 0;
+    let computer_wins = 0;
+    let player_wins = 0;
 
     if (playerChoice==computerChoice) {
+        tie++;
         document.getElementById("result").innerHTML = "Its a tie!"
     } else if(playerChoice>computerChoice) {
+        player_wins++;
         document.getElementById("result").innerHTML = "Player wins! Computer choose: " + computerChoiceToString(computerChoice);
     } else {
+        computer_wins++;
         document.getElementById("result").innerHTML = "Computer wins! It choose: " + computerChoiceToString(computerChoice);
     }
+    return [tie, computer_wins, player_wins];
+}
+
+function countScore() {
+
+    let scores = playRound();
+
+    let current_tie = parseInt(document.getElementById("tie").innerHTML);
+    let current_computer = parseInt(document.getElementById("computer_score").innerHTML);
+    let current_player = parseInt(document.getElementById("player_score").innerHTML);
+
+
+    if (isNaN(current_tie)) {
+        current_tie = 0;
+    }
+    if (isNaN(current_computer)) {
+        current_computer = 0;
+    }
+    if (isNaN(current_player)) {
+        current_player = 0;
+    }
+
+    let new_tie = current_tie+scores[0];
+    let new_computer = current_computer+scores[1];
+    let new_player = current_player+scores[2];
+
+    document.getElementById("tie").innerHTML = new_tie.toString();
+    document.getElementById("computer_score").innerHTML = new_computer.toString();
+    document.getElementById("player_score").innerHTML = new_player.toString();
 
 }
+
+const btn = document.getElementById("play_button");
+btn.addEventListener("click", getPlayerChoice);
+btn.addEventListener("click", playRound);
+btn.addEventListener("click", countScore);
