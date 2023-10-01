7c721841db9f4170a4f647d6b4e50932d8f23082
musimarcin <marcinm994@gmail.com>
2023-10-01 12:30:33 +0200

    Fixed bug when computer always wins. Game works as intended.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,6 @@ const inputs = document.querySelectorAll("input");
 
 function getPlayerChoice() {
     playRound(this.id, getComputerChoice());
-    countScore();
 }
 
 inputs.forEach(input => input.addEventListener('click', getPlayerChoice));
@@ -37,35 +36,12 @@ function computerChoiceToString(computerString) {
 
 function playRound(playerChoice, computerChoice) {
 
-    computerChoice = getComputerChoice();
 
     if (playerChoice==0 && computerChoice==2) {
         computerChoice = -computerChoice;
     } else if (computerChoice==0 && playerChoice==2) {
         playerChoice = -playerChoice;
     }
-
-    let tie = 0;
-    let computer_wins = 0;
-    let player_wins = 0;
-
-
-        if (playerChoice == computerChoice) {
-            tie++;
-            document.getElementById("text").textContent = "Its a tie!"
-        } else if (playerChoice > computerChoice) {
-            player_wins++;
-            document.getElementById("text").textContent = "Player wins! Computer choose: " + computerChoiceToString(computerChoice);
-        } else {
-            computer_wins++;
-            document.getElementById("text").textContent = "Computer wins! It choose: " + computerChoiceToString(computerChoice);
-        }
-        return [tie, computer_wins, player_wins];
-}
-
-function countScore() {
-
-    let scores = playRound();
 
     let current_tie = parseInt(document.getElementById("tie").textContent);
     let current_computer = parseInt(document.getElementById("computer_score").textContent);
@@ -82,9 +58,22 @@ function countScore() {
         current_player = 0;
     }
 
-    let new_tie = current_tie+scores[0];
-    let new_computer = current_computer+scores[1];
-    let new_player = current_player+scores[2];
+
+        if (playerChoice == computerChoice) {
+            current_tie++;
+            document.getElementById("text").textContent = "Its a tie!"
+        } else if (playerChoice > computerChoice) {
+            current_player++;
+            document.getElementById("text").textContent = "Player wins! Computer choose: " + computerChoiceToString(computerChoice);
+        } else {
+            current_computer++;
+            document.getElementById("text").textContent = "Computer wins! It choose: " + computerChoiceToString(computerChoice);
+        }
+
+
+    let new_tie = current_tie;
+    let new_computer = current_computer;
+    let new_player = current_player;
 
     document.getElementById("tie").textContent = new_tie.toString();
     document.getElementById("computer_score").textContent = new_computer.toString();
